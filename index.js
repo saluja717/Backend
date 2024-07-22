80bcf273af449cb362ca3d55993fcfc74805a122
Manpreet Singh Saluja <68748154+saluja717@users.noreply.github.com>
2024-07-22 20:07:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(cookieParser())
 app.use(
     cors({
         origin: "*",
-        credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     })
