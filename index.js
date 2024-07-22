2fff3ac85096eb576c576bcdba018ed837567ab2
Manpreet Singh Saluja <68748154+saluja717@users.noreply.github.com>
2024-07-22 22:03:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,21 +13,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use(
-    cors()
+    cors({
+       origin:'https://frontend-lemon-rho-68.vercel.app',
+       credentials:true
+    })
 )
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://frontend-lemon-rho-68.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'false');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  if (req.method === 'OPTIONS') {
-    // Handle preflight requests
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
 app.use("/insert", InsertData);
 app.use("/", GetData);
