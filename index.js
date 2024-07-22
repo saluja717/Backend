9dab0a1d488c8b804456caca39e42762df9fb260
Manpreet Singh Saluja <68748154+saluja717@users.noreply.github.com>
2024-07-22 20:10:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(cookieParser())
 
 app.use(
     cors({
-        origin: "*",
+        origin:true,
+        credentials:true,
         allowedHeaders: ['Content-Type', 'Authorization'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     })
@@ -22,7 +23,7 @@ app.use(
 
 // Middleware to set additional headers
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'true');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
