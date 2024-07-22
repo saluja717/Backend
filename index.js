7e18e9334e6ee1b9e1c258163d370147818b3f0b
Manpreet Singh Saluja <68748154+saluja717@users.noreply.github.com>
2024-07-22 19:13:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 app.use(
     cors({
-        origin: "https://frontend-opal-theta.vercel.app/",
+        origin: "*",
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
@@ -23,7 +23,7 @@ app.use(
 
 // Middleware to set additional headers
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://frontend-opal-theta.vercel.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
