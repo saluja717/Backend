ce2af29e1cbcf89c210b13c1044d9e52916c8510
Manpreet Singh Saluja <68748154+saluja717@users.noreply.github.com>
2024-07-22 20:23:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(
     cors({
         origin:"https://frontend-lemon-rho-68.vercel.app",
         credentials:true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
     })
 )
 
